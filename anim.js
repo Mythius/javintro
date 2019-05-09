60c3e0d6df79fb879b7a62635eb8ea9894655ad5
Matthias Southwick <33238125+Mythius@users.noreply.github.com>
2019-05-09 17:04:02 -0600

    Delete anim.js

--- anim.js
+++
@@ -1,7 +0,0 @@
-function Animation(fr,dur,fn){
-var frames = fr,duration = dur,fnc=fn,loop=0,
-frame = function(){fnc();loop++;
-if(loop<duration){requestAnimationFrame(frame);}}
-this.start=function(){
-loop=0;
-frame();}}
